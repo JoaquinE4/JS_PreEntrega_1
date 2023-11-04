141dcd9265d11c0a1000571e19ba1aa94b58a01e
Joaco.dEx <casarotajoachin@gmail.com>
2023-11-04 14:13:02 -0300

    Pre-entrega n°1 Completa

--- js/app.js
+++ js/app.js
@@ -1,44 +1,84 @@
-console.log("Bienvenido");
+let nameUser;
 
-const ageUser = parseInt(prompt("Ingrese edad"));
-const nameUser = prompt("Ingrese nombre de usuario");
-const emailUser = prompt("ingrese email");
-const passUser = prompt("Ingrese contraseña");
+const login = () => {
 
-if (ageUser && nameUser && emailUser && passUser) {
+    const ageUser = parseInt(prompt("Ingrese edad"));
 
-} else if ((!ageUser && nameUser && emailUser && passUser)) {
-    alert("Ingrese Edad");
-} else if ((ageUser && !nameUser && emailUser && passUser)) {
-    alert("Ingrese Nombre de usuario");
-} else if ((ageUser && nameUser && !emailUser && passUser)) {
-    alert("Ingrese Email");
-} else if ((ageUser && nameUser && emailUser && !passUser)) {
-    alert("Ingrese Contraseña");
-} else {
-    alert("Complete los campos");
-}
-
-
-function ageIf() {
-    if (ageUser >= 18) {
-        alert("Bienvenido " + nameUser);
-    } else {
-        alert("No puedes acceder");
+    while (true) {
+        if (ageUser >= 18) {
+            break;
+        } else {
+            alert("Eres menor, no puedes acceder");
+            ageUser = parseInt(prompt("Ingrese edad nuevamente"));
+        }
+        return;
     }
-    return
+
+    nameUser = prompt("Ingrese nombre de usuario");
+    const emailUser = prompt("Ingrese email");
+    const passUser = prompt("Ingrese contraseña");
+
+    while (true) {
+        if (nameUser && emailUser && passUser) {
+            alert("Ingreso exitoso");
+            mensaje();
+            menuDeAdivinanzas();
+        } else if (!nameUser) {
+            alert("Complete el Nombre");
+            nameUser = prompt("Intenta nuevamente");
+        } else if (!emailUser) {
+            alert("Complete el Email");
+            emailUser = prompt("Intenta nuevamente");
+        } else if (!passUser) {
+            alert("Complete la contraseña");
+            passUser = prompt("Intenta nuevamente");
+        } else {
+            alert("No podes aceder sin completar los campos requeridos")
+        }
+        return;
+    }
 }
 
-ageIf();
+const mensaje = () => { alert("Bienvenido" + " " + nameUser + "!") }
 
-alert("ahora pasa esto")
+function menuDeAdivinanzas() {
+
+    const respuesta = confirm("¿Queres Continuar?");
+
+    if (respuesta) {
+        alert("Que Bueno!")
+
+    } else {
+        alert("¡Que Lastima!");
+
+    }
 
 
+    if (respuesta === true) {
+
+        const acertijo = "¿De que color es el caballo blanco de san martin?";
+
+        alert(acertijo);
 
 
+        const respuesta = prompt("¿Cuál es tu respuesta?");
+
+        if (respuesta === "Blanco") {
+
+            alert("Corrrexto")
+
+        } else {
+            alert("Respuesta incorrecta. La respuesta es Blanco.");
+        }
 
 
+    } else {
+        alert("¡Volve Cuando quieras!")
 
+    }
+}
+
+login();
 
 
 
